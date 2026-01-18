17aba00255d0c05a10fbdf3bfee0caabeb31a2b2
neeluux <haydenju@protonmail.com>
2026-01-18 08:17:55 -0500

    Formatting fixes

--- src/color.h
+++ src/color.h
@@ -4,7 +4,7 @@
 #include <raylib.h>
 
 #define LIMEGREEN		(Color)	{ 2, 249, 9, 255 }
-#define MODERATEPINK    	(Color)	{ 228, 91, 217, 255 }
+#define MODERATEPINK		(Color)	{ 228, 91, 217, 255 }
 #define RAINFORESTGREEN		(Color)	{ 0, 158, 117, 255 }
 #define MODERATEBLUE		(Color)	{ 63, 85, 198, 255}
 #define BERRYPURPLE		(Color)	{ 99, 85, 137, 255 }
